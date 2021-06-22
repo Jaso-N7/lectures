c715bd3654379dd6f5203c907659a33229a402e9
Jason Robinson <Jason.Robinson@mayberryinv.com>
2021-06-22 15:42:39 -0500

    Use return-from in a function to return the SID of any student who hasn't attmpted any modules at all.

--- lecture-8/src/main.lisp
+++ lecture-8/src/main.lisp
@@ -69,7 +69,7 @@
     (let ((yn (read-line)))
       (if (char= (char yn 0) #\n)
 	  (view-students)
-	  (register-student)))))
+	  (register-students)))))
 
 (defun view-students ()
   "View all students registered so far."
@@ -97,8 +97,22 @@
 	   (format t "~&No record found, confirm spelling.~%Nothing deleted.")))))
 
 (defun un-enrolled-students ()
+  "Return the list of SIDs of any student who hasn't attempted any modules at all."
+  (let ((unenrolled ()))
+    (maphash #'(lambda (key value)
+		 (when (null (students-modules value))
+		   (push (students-sid value) unenrolled)))
+	     *student-table*)
+    unenrolled))
+
+(defun un-enrolled-student ()
   "Return the SID of any student who hasn't attempted any modules at all."
-  (error "Not yet implemented."))
+  (maphash #'(lambda (key value)
+	       (when (null (students-modules value))
+		 (return-from un-enrolled-student
+		   (students-sid value))))
+	   *student-table*))
+
 
 (defun create-modules ()
   "Create the modules for students to enroll."
@@ -154,5 +168,5 @@ Returns the table of Students."
 #|
 !!! TODO: As before, write functions to name the three students who have the highest marks, or to spot which lecturer fails most of their students.
 
-Use return-from in a function to return the SID of any student who hasn't attmpted any modules at all. 
+
 |#
