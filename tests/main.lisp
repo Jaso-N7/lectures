ac370428ee27cee1dd97f202916001b77fc6c119
Jason Robinson <json.robinson@gmail.com>
2021-07-11 08:54:03 -0500

    Included more Unit Tests for LEC 10 BETTER-EQUAL

--- tests/main.lisp
+++ tests/main.lisp
@@ -20,15 +20,38 @@
 (defun run-unit-tests ()
   "Unit Tests."
   (terpri)
-  (princ "Lecture 10 Unit Tests:")
+  (print "Lecture 10 Unit Tests:")
   (terpri)
-  (is= T
-       (lec-10::better-equal "aardvark" "aardvark" "aardvark" "aardvark"))
-  (is= NIL
-       (lec-10::better-equal "aardvark" "aardvark" "goat" "aardvark")))
+  (quickcheck
+    (named "String Comparison" 
+      (is= T
+	   (lec-10::better-equal "aardvark" "aardvark" "aardvark" "aardvark"))
+      (isnt= T
+	     (lec-10::better-equal "aardvark" "aardvark" "goat" "aardvark")))
+    (named "Number Comparison"
+      (is= T
+	   (lec-10::better-equal 1 1 1 1 1 1 1 1 1 1 ))
+      (isnt= T
+	     (lec-10::better-equal 1 1 1 5 5 5 42 42 5 5)))
+    (named "Symbol Comparison"
+      (is= T
+	   (lec-10::better-equal 'a 'a 'a))
+      (isnt= T
+	   (lec-10::better-equal 'a 'a 'a 'b)))
+    (named "Mixed arguments."
+      (isnt= T
+	     (lec-10::better-equal #\I #\s #\n #\t))
+      (isnt= T
+	     (lec-10::better-equal 'a #\b "c" 1 2.0 pi)))
+    (named "Amount of arguments passed"
+      (is= T (lec-10::better-equal))
+      (is= T (lec-10::better-equal "aardvark")))))
+
 
 (defun run-quickchecks ()
   "Property based tests"
-  (format t "~&Lecture 10 PBTs:~%"))
+  (quickcheck
+    (named "Lecture 10 PBTs"
+      (is= 1 1))))
 
 
