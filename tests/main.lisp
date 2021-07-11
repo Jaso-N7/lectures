20d39bfc0c241ffc02a9d5645ced1316fb47bd1d
Jason Robinson <json.robinson@gmail.com>
2021-07-11 15:45:51 -0500

    Revised tests for LEC-10::BETTER-EQUAL

--- tests/main.lisp
+++ tests/main.lisp
@@ -21,7 +21,6 @@
 		  :collect
 		  (+ min-number
 		     (random (- max-number min-number)))))
-    (char (make-list length :initial-element init-element))
     (otherwise (list 0 1 2 3 4 5 6 7 8 9
 		     'a 'b 'c 'd 'e 'f 'g))))
   
@@ -38,21 +37,9 @@
   (print "Lecture 10 Unit Tests:")
   (terpri)
 
-  (named "BETTER-EQUAL: String Comparison" 
-    (is= T
-	 (lec-10::better-equal "aardvark" "aardvark" "aardvark" "aardvark"))
-    (isnt= T
-	   (lec-10::better-equal "aardvark" "aardvark" "goat" "aardvark")))
-  (named "BETTER-EQUAL: Symbol Comparison"
-    (is= T
-	 (lec-10::better-equal 'a 'a 'a))
-    (isnt= T
-	   (lec-10::better-equal 'a 'a 'a 'b)))
-  (named "BETTER-EQUAL: Mixed arguments."
-    (isnt= T
-	   (lec-10::better-equal #\I #\s #\n #\t))
-    (isnt= T
-	   (lec-10::better-equal 'a #\b "c" 1 2.0 pi)))
+  (named "BETTER-EQUAL: Number Comparison"
+    (is= T (lec-10::better-equal 14 14))
+    (isnt= T (lec-10::better-equal 14 15)))
   (named "BETTER-EQUAL: Zero to one arguments of any type passed"
     (is= T (lec-10::better-equal))
     (is= T (lec-10::better-equal "aardvark"))
@@ -65,24 +52,44 @@
   "Property based tests"
   (print "Lecture 10 PBTs")
   (terpri)
-  (let ((list-length (random (1+ 42))))
-    (for-all ((str #'a-string)
-	      (str2 #'a-string))
-      (let* ((lstr (random-list list-length :init-element str))
-	     (lstr-rando (adjoin str2 lstr))) 
-	(named "LEC-10::BETTER-EQUAL String Comparison"
-	  (test (apply #'lec-10::better-equal lstr))
-	  (is lec-10::better-equal lstr-rando))))
 
-    (let ((list-nums '(1 1 1 1 1 1 1 1 1 1 1)))
+    (named "BETTER-EQUAL String Comparison"
+      (is= T
+	   (lec-10::better-equal "aardvark" "aardvark" "aardvark" "aardvark"))
+      (isnt= T
+	     (lec-10::better-equal "aardvark" "aardvark" "goat" "aardvark"))
+      (for-all ((strings (a-list #'a-string)))
+	  (is lec-10::better-equal strings)))
+
+    (named "BETTER-EQUAL: Number Comparison"
+      (is= T (lec-10::better-equal 1 1 1 1 1 1 1 1 1 1 1))
       (for-all ((rand-nums (a-list an-integer)))
-	(named "LEC-10 BETTER-EQUAL: Number Comparison"
-	  (is lec-10::better-equal list-nums)
 	  ;; Unit Tests already covers this case
-	  (only-if (>= (length rand-nums) 2)
+	  (only-if (> (length rand-nums) 2)
 		   (isnt= T
-			  (apply #'lec-10::better-equal rand-nums))))))))
-;	  (test (apply #'lec-10::better-equal rand-nums)))))))
+			  (apply #'lec-10::better-equal rand-nums)))))
+  
+  (named "BETTER-EQUAL (PBT): Mixed arguments."
+    (isnt= T
+	   (lec-10::better-equal #\I #\s #\n #\t))
+    (isnt= T
+	   (lec-10::better-equal 'a #\b "c" 1 2.0 pi))
+      (for-all ((syms (a-list #'a-symbol))
+		(nums (a-list an-integer))
+		(strings (a-list #'a-string))
+		(chars (a-list #'a-char)))
+	(let ((mixed (append syms nums strings chars)))
+	  (only-if (> (length mixed) 2)
+		   (isnt= T (apply #'lec-10::better-equal mixed))))))
+  
+    (named "BETTER-EQUAL: Symbol Comparison"
+      (is= T (lec-10::better-equal 'a 'a 'a))
+      (for-all ((symbols (a-list #'a-symbol)))
+	(only-if (> (length symbols) 2)
+		 (isnt= T (apply #'lec-10::better-equal symbols))))))
+		 
+
+
       
 
       
