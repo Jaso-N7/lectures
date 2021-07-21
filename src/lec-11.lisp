813c15833ecb0ee89f2e43bfe92f4a20637605d6
Jason S. Robinson <jason.robinson@mayberryinv.com>
2021-07-20 21:37:00 -0500

    Completed exercise 1.

--- src/lec-11.lisp
+++ src/lec-11.lisp
@@ -6,3 +6,13 @@ Also includes the exercises from Paul Graham's ANSI CL, Chapter 10."))
 
 (in-package "LECTURES-11")
 
+;; 1.(a)
+(let ((x 'a)
+      (y 'b)
+      (z '(c d)))
+  (defun 1a ()
+    `(,z ,x z))
+  (defun 1b ()
+    `(x ,y ,@z))
+  (defun 1c ()
+    `((,@z ,x) z)))
